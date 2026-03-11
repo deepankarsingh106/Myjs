84f4f7f8f58041ac27f5ba38c7ba0ac3018d1a37
Deepankar Singh <deepankarsingh106@gmail.com>
2026-03-11 18:48:10 +0000

    Varialble Learing

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail =  "Deepankar@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountId = 2 Can't Do that
+accountEmail = "ds@ds.com"
+accountPassword = "12123"
+accountCity = "Delhi"
+let accountState;
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
