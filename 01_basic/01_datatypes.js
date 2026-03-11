e814c8b8729dc3d40f023951183872322fde003f
Deepankar Singh <deepankarsingh106@gmail.com>
2026-03-11 19:12:31 +0000

    Learning Diffrent Data Types

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";
+/*treat all JS code as newer version*/
+//alert("Hello") we are using node js not browsers
+console.log(3+2)
+// code readibility should be high
+console.log("Deepankar")
+
+let name = "Deepankar"
+let age = 18
+let isLoggedIn = false
+
+//Number --> 2^^ 53
+//bigint --> for large number
+//string -> " " 
+//boolean = true/false
+//null --> standalone values that it is type it is empty
+//undefined -->  
+// symbol --> unique
+
+
+// NON Primimtive data types --> object 
+// undefined -- > it is type 
+console.log(typeof undefined)
