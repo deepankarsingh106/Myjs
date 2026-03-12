0995109844dac3407625113f7905ace9f9e9f594
Deepankar Singh <deepankarsingh106@gmail.com>
2026-03-12 10:50:23 +0000

    Converion of operator

---
+++ 01_basic/01_conversion.js
@@ -0,0 +1,17 @@
+let score = "hitesh"
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = 1
+// let booleansisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleansisLoggedIn);
+
+let someNumber = 22;
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
