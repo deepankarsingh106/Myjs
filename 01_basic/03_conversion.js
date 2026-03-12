a8e26453c19cc3e2513493d6bd9c515a9ec1fd67
Deepankar Singh <deepankarsingh106@gmail.com>
2026-03-12 13:22:36 +0000

    change

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -11,7 +11,26 @@ let score = "hitesh"
 // let booleansisLoggedIn = Boolean(isLoggedIn)
 // console.log(booleansisLoggedIn);
 
-let someNumber = 22;
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// let someNumber = 22;
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+// let v = 3
+// let negv = -v
+// console.log(negv);
+
+// str1 = "Hi"
+// str2 = " World"
+// str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2");
+
+// console.log(2 % 3 + 7 - 9 * 7)
+console.log(true)
+console.log(+"")
