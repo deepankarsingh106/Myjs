4d51f1033b75f029c04cfefb54c26768305370b1
Deepankar Singh <deepankarsingh106@gmail.com>
2026-03-28 13:40:23 +0000

    Now Fully Code

--- 01_basic/04_Comparision.js
+++ 01_basic/04_Comparision.js
@@ -9,12 +9,18 @@
 // console.log("2" > 1)
 // console.log("02" > 1)
 
-console.log(null > 0)
-console.log(null == 0)
-console.log(null < 0)
-console.log(null >= 0)
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null < 0)
+// console.log(null >= 0)
 
-console.log(undefined == 0)
-console.log(undefined == "")
+//console.log(undefined == 0)
+// console.log(undefined == "")
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
 
-//
+// === ---> Strict Check
+console.log("2" == 2);
+console.log("2" === 2);
