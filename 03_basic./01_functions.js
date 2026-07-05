dc1249b78e3dea77b4ed47894d2b4db57b83a0c9
Deepankar Singh <deepankarsingh106@gmail.com>
2026-07-05 14:48:26 +0000

    Function

---
+++ 03_basic./01_functions.js
@@ -0,0 +1,26 @@
+// console.log("D")
+// console.log("E")
+// console.log("E")
+// console.log("P")
+
+
+// another way of doing this 
+function sayMyname(){
+    // function scope and defintion
+    console.log("D")
+    console.log("E")
+    console.log("E")
+    console.log("P")
+
+}
+
+// sayMyname -> function ka refrences - output nothing
+// sayMyname() -> function ka executions
+
+
+//sayMyname()
+
+// 
+function add(number1,number2){
+
+}
